b5b96b6b33e0916a523d30a23e1936b9d7105396
Christopher Spinrath <netbook@spinrath.net>
2012-02-29 22:40:16 +0100

    Init

---
+++ tp_widgets.lua
@@ -0,0 +1,61 @@
+local io = io
+local string = string
+local math = math
+
+module("tp_widgets")
+
+local battery_path = "/sys/devices/platform/smapi/BAT0/"
+
+function exec_command(command)
+	local stream = io.popen(command)
+	if not stream then do return nil end end
+	
+	local result = stream:read('*a')
+
+	io.close(stream)
+
+	return result
+end
+
+function get_battery_state()
+	local catcommand = "cat " .. battery_path
+
+	local percent = exec_command(catcommand .. "remaining_percent")
+	local maxpercent = exec_command(catcommand .. "stop_charge_thresh")
+	local state = exec_command(catcommand .. "state");
+	
+	local text = "|B: "
+
+	if string.find(state,"discharging") then
+		local time = exec_command(catcommand .. "remaining_running_time")
+		local hours = math.floor(time/60)
+		local minutes = time - hours * 60
+		text  = text .. string.format("%2.f",hours) .. ":" .. string.format("%2.f", minutes )
+
+	elseif string.find(state,"charging") then
+		text = text .. "charging"
+	else
+		text = text .. "ac power"
+	end
+	
+	local percentstr = string.format("%2.f", percent)
+	local maxstr = string.format("%2.f", maxpercent)
+	local text  = text .. " ( " .. percentstr ..  "/" .. maxstr .. "% )"
+
+	return text
+end
+
+function get_power_state()
+	local catcommand = "cat " .. battery_path
+
+	local powerinfo = exec_command(catcommand .. "power_now") * 1;
+	local text = " |P:"
+
+	if powerinfo >= 0 then
+		return text .. " --"
+	end
+
+	text = text .. string.format("%6.f",math.abs(powerinfo)) .. " mW"
+
+	return text
+end
