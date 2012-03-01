5e56682ab298cb5a1192258cde8c0e8918cf4ca8
Daniel Schulte <daniel@schultes-home.de>
2012-03-01 20:56:24 +0100

    Added the option to use a short display format and replaced tabs with spaces

--- tp_widgets.lua
+++ tp_widgets.lua
@@ -13,130 +13,153 @@ default_text = "no tp_smapi found"
 default_power_update_freq = 10
 default_battery_update_freq = 120
 
-battery_discharging_format = "|B:%02.f:%02.f "
-battery_charging_format = "|B: charging "
-battery_ac_format = "|B: ac power "
-battery_percent_format = "( %02.f/%02.f%% )"
+-- Normal format strings
+battery_discharging_format = "B: %02.f:%02.f"
+battery_charging_format = "B: charging"
+battery_ac_format = "B: ac power"
+battery_percent_format = " ( %02.f/%02.f%% )"
 
-power_discharging_format = " |P:%6.f mW"
-power_charging_format = " |P: --"
+power_discharging_format = "P:%6.f mW"
+power_charging_format = "P: --"
+
+-- Shortened format strings
+battery_discharging_short_format = "B: %02.f:%02.f"
+battery_charging_short_format = "B: C"
+battery_ac_short_format = "B: AC"
+battery_percent_short_format = " (%02.f/%02.f%%)"
+
+power_discharging_short_format = "P:%6.f mW"
+power_charging_short_format = "P: --"
 
 local metasettings = {}
 
 local function create_widget_helper(timer_func,update_freq)
-	local w = widget({ type = "textbox" })
-	metasettings[w] = {}
-	metasettings[w].widget = w
-	metasettings[w].timer = timer({ timeout = update_freq })
-	metasettings[w].update = function() timer_func(metasettings[w]) end			
-	metasettings[w].timer:add_signal("timeout", metasettings[w].update)
+    local w = widget({ type = "textbox" })
+    metasettings[w] = {}
+    metasettings[w].widget = w
+    metasettings[w].timer = timer({ timeout = update_freq })
+    metasettings[w].update = function() timer_func(metasettings[w]) end            
+    metasettings[w].timer:add_signal("timeout", metasettings[w].update)
 
-	metasettings[w].init = function() metasettings[w].timer:start() metasettings[w].update() end
+    metasettings[w].init = function() metasettings[w].timer:start() metasettings[w].update() end
 
-	return w
+    return w
 end
 
 function update(widget)
-	if metasettings[widget] ~= nil then
-		metasettings[widget].update()
-	end
+    if metasettings[widget] ~= nil then
+        metasettings[widget].update()
+    end
 end
 
 function set_custom_update(widget,func)
-	if metasettings[widget] ~= nil then
-		metasettings[widget].custom_update = func
-	end
+    if metasettings[widget] ~= nil then
+        metasettings[widget].custom_update = func
+    end
 end
 
 function getMetaObject(widget)
-	return metasettings[widget]
+    return metasettings[widget]
 end
 
-function create_battery_widget(update_freq)
-	local w = create_widget_helper(update_battery_state, update_freq or default_battery_update_freq)
+function create_battery_widget(use_short_format, update_freq)
+    local w = create_widget_helper(update_battery_state, update_freq or default_battery_update_freq)
+    
+    if use_short_format then
+        metasettings[w].discharging_format = battery_discharging_short_format
+        metasettings[w].charging_format = battery_charging_short_format
+        metasettings[w].ac_format = battery_ac_short_format
+        metasettings[w].percent_format = battery_percent_short_format
+    else
+        metasettings[w].discharging_format = battery_discharging_format
+        metasettings[w].charging_format = battery_charging_format
+        metasettings[w].ac_format = battery_ac_format
+        metasettings[w].percent_format = battery_percent_format
+    end
+    
+    metasettings[w].init()
 
-	metasettings[w].discharging_format = battery_discharging_format
-	metasettings[w].charging_format = battery_charging_format
-	metasettings[w].ac_format = battery_ac_format
-	metasettings[w].percent_format = battery_percent_format
-	metasettings[w].init()
-
-	return w
+    return w
 end
 
-function create_power_widget(update_freq)
-	local w = create_widget_helper(update_power_state, update_freq or default_power_update_freq)
+function create_power_widget(use_short_format, update_freq)
+    local w = create_widget_helper(update_power_state, update_freq or default_power_update_freq)
 
-	metasettings[w].discharging_format = power_discharging_format
-	metasettings[w].charging_format = power_charging_format
-	metasettings[w].init()
+    if use_short_format then
+        metasettings[w].discharging_format = power_discharging_short_format
+        metasettings[w].charging_format = power_charging_short_format
+    else
+        metasettings[w].discharging_format = power_discharging_format
+        metasettings[w].charging_format = power_charging_format
+    end
 
-	return w
+    metasettings[w].init()
+
+    return w
 end
 
 local function exec_command(command)
-	local stream = io.popen(command)
-	if not stream then do return nil end end
-	
-	local result = stream:read('*a')
+    local stream = io.popen(command)
+    if not stream then do return nil end end
+    
+    local result = stream:read('*a')
 
-	io.close(stream)
+    io.close(stream)
 
-	return result
+    return result
 end
 
 function update_battery_state(info)
-	local catcommand = "cat " .. battery_path
-	
-	if info.max_percent == nil then
-		info.max_percent = exec_command(catcommand .. "stop_charge_thresh")
-	end
+    local catcommand = "cat " .. battery_path
+    
+    if info.max_percent == nil then
+        info.max_percent = exec_command(catcommand .. "stop_charge_thresh")
+    end
 
-	info.percent = exec_command(catcommand .. "remaining_percent")
-	local state = exec_command(catcommand .. "state");
-	
-	local text = ""
+    info.percent = exec_command(catcommand .. "remaining_percent")
+    local state = exec_command(catcommand .. "state");
+    
+    local text = ""
 
-	if string.find(state,"discharging") then
-		local time = exec_command(catcommand .. "remaining_running_time")
-		info.hours = math.floor(time/60)
-		info.minutes = time - info.hours * 60
-		info.is_discharging = true
-		info.is_charging = false
-		text  = string.format(info.discharging_format,info.hours,info.minutes)
+    if string.find(state,"discharging") then
+        local time = exec_command(catcommand .. "remaining_running_time")
+        info.hours = math.floor(time/60)
+        info.minutes = time - info.hours * 60
+        info.is_discharging = true
+        info.is_charging = false
+        text  = string.format(info.discharging_format,info.hours,info.minutes)
+    elseif string.find(state,"charging") then
+        info.is_discharging = false
+        info.is_charging = true
+        text = string.format(info.charging_format)
+    else
+        info.is_discharging = false
+        info.is_charging = false
+        text = string.format(info.ac_format)
+    end
+    
+    local text = text .. string.format(info.percent_format,info.percent,info.max_percent)
+    
+    if info.custom_update ~= nil then
+        info.custom_update(info)
+    end
 
-	elseif string.find(state,"charging") then
-		info.is_discharging = false
-		info.is_charging = true
-		text = string.format(info.charging_format)
-	else
-		info.is_discharging = false
-		info.is_charging = false
-		text = string.format(info.ac_format)
-	end
-	
-	local text = text .. string.format(info.percent_format,info.percent,info.max_percent)
-	
-	if info.custom_update ~= nil then
-		info.custom_update(info)
-	end
-
-	info.widget.text = text
+    info.widget.text = text
 end
 
 function update_power_state(info)
-	local catcommand = "cat " .. battery_path
+    local catcommand = "cat " .. battery_path
 
-	info.value = exec_command(catcommand .. "power_now") * 1;
+    info.value = exec_command(catcommand .. "power_now") * 1;
 
-	if info.value >= 0 then
-		info.widget.text = string.format(info.charging_format,math.abs(info.value))
-		return
-	end
-	
-	if info.custom_update ~= nil then
-		info.custom_update(info)
-	end
+    if info.value >= 0 then
+        info.widget.text = string.format(info.charging_format,math.abs(info.value))
+        return
+    end
+    
+    if info.custom_update ~= nil then
+        info.custom_update(info)
+    end
 
-	info.widget.text = string.format(info.discharging_format,math.abs(info.value))
+    info.widget.text = string.format(info.discharging_format,math.abs(info.value))
 end
